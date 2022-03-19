5a1c1204ad2505dd8a79983d9e50ca459de11640
Martin <mkmartinoes@gmail.com>
2022-03-19 08:03:07 +0300

    ft: price prototype

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,25 @@ function Pizza(size, topings, feel, price){
     this.feel = feel;
 }
 
+let pizzaSize = [
+    ['small', 700],
+    ['medium', 900],
+    ['large', 1200]
+]
+
+let top = [50, 60, 30, 70]
+
+let crust = [78]
+
+
+Pizza.prototype.price = function(){
+    let mySize = this.size;
+    let myTop = this.size;
+    let myCrust = this.feel
+    let myPrice = pizzaSize[mySize] + top[myTop];
+    return myPrice
+}
+
+let order1 = new Pizza (1, 2);
+
+console.log(order1.price())
